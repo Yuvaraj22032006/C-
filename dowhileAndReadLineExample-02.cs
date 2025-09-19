1630f89449e74bae33b7cc5255cbc4e2179aa97b
Yuvaraj22032006 <myuvaraj357@gmail.com>
2025-09-19 19:17:10 +0530

    Add files via upload

---
+++ dowhileAndReadLineExample-02.cs
@@ -0,0 +1,25 @@
+﻿string? roleName;
+string roleEntered = "";
+bool entry = false;
+
+do
+{
+    Console.WriteLine("Enter your role name (Administor, Manager, or User)");
+    roleName = Console.ReadLine();
+    if (roleName != null)
+    {
+        roleEntered = roleName.Trim();
+    }
+
+    if (roleEntered.ToLower() == "administrator" || roleEntered.ToLower() == "Manager" || roleEntered.ToLower().ToLower() == "User")
+    {
+        entry = true;
+    }
+    else
+    {
+        Console.WriteLine($"The role name you entered, \"{roleEntered}\" is not valid.");
+    }
+} while (entry == false);
+
+Console.WriteLine($"Your input value({roleEntered}) has been accepted.");
+roleName = Console.ReadLine();
