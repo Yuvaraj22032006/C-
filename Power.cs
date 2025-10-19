8df525d6d691e20602ebb41312edb58e5258a446
Yuvaraj22032006 <myuvaraj357@gmail.com>
2025-10-19 20:00:08 +0530

    Add files via upload

---
+++ Power.cs
@@ -0,0 +1,25 @@
+﻿using System.IO.Pipelines;
+
+string? a = "";
+string? b = "";
+static int multi(int baseNum, int powNum)
+{
+    int Result = 1;
+    for (int i = 0; i < powNum; i++)
+    {
+        Result = Result * baseNum;
+    }
+    return Result;
+}
+Console.WriteLine("Enter Base:");
+a = Console.ReadLine();
+int num1 = Convert.ToInt32(a);
+
+Console.WriteLine("Enter Power:");
+b = Console.ReadLine();
+int num2 = Convert.ToInt32(b);
+
+
+Console.Write(multi(num1, num2));
+
+
