6525bae50cbeb03f85b32498c6fac27493e510ff
Yuvaraj22032006 <myuvaraj357@gmail.com>
2025-10-19 19:40:43 +0530

    Add files via upload

---
+++ Gussing Game.cs
@@ -0,0 +1,32 @@
+﻿string guessWord = "ZEX";
+string? guess = "";
+int guessCount = 0;
+int guessLimit = 3;
+bool outOfGussess = false;
+
+
+do
+{
+    if (guessCount < guessLimit)
+    {
+        Console.Write("Enter your guess:");
+        if (guess != null) {
+            guess = Console.ReadLine().ToUpper();
+        }
+        guessCount++;
+        
+    }
+    else
+    {
+        outOfGussess = true;
+    }
+
+    if (guess == guessWord)
+    {
+        Console.WriteLine("You win!");
+    }
+    else if(outOfGussess)
+    {
+        Console.WriteLine("You lose!");
+    }
+}while (guess != guessWord && !outOfGussess) ;
