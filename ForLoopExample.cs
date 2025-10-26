41f4b394ea2d6a7378a41d17822c623f2f078b35
Yuvaraj22032006 <myuvaraj357@gmail.com>
2025-10-26 21:56:32 +0530

    Add files via upload

---
+++ ForLoopExample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class ForLoopExample
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What do you want to repeat?: ");
+            string message = Console.ReadLine();
+
+            Console.WriteLine("How many times do you want to repeat it?: ");
+            int repeatCount = Convert.ToInt32(Console.ReadLine());
+
+            if (repeatCount <= 0) {
+                Console.WriteLine("The number must be greater than zero.");
+            } else {
+                for (int i = 0; i < repeatCount; i++)
+                {
+                    Console.WriteLine(message);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
