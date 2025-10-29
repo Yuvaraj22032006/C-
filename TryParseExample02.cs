5d124821a65ece809d69a4597908f7eec29302e5
Yuvaraj22032006 <myuvaraj357@gmail.com>
2025-10-29 21:13:40 +0530

    Add files via upload

---
+++ TryParseExample02.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class ForLoopExample
+    {
+        static void Main(string[] args)
+        {
+            bool success = true;
+
+            while (success)
+            {
+                Console.WriteLine("Enter a no.: ");
+                string numInput = Console.ReadLine();
+
+                if (int.TryParse(numInput, int num);)
+                {
+                    Console.WriteLine(num);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
