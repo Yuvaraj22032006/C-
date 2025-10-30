df954a8f766b74c7bb4efb6958feb03facd1de43
Yuvaraj22032006 <myuvaraj357@gmail.com>
2025-10-30 21:24:49 +0530

    Add files via upload

---
+++ multiplicationTable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class ForLoopExample
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number to display its multiplication table:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} x {1} = {2}", number, i, number * i);
+                int multiply = number * i;
+                Console.WriteLine($"{number} x {i} = {multiply}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
