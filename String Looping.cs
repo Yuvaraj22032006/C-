edceebbd5880d170b315ee0dd67ab0c11986aa6a
Yuvaraj22032006 <myuvaraj357@gmail.com>
2025-11-13 19:48:08 +0530

    Add files via upload

---
+++ String Looping.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace C_PROJECTS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string : ");
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.Write(input[i]);
+                Thread.Sleep(100);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].Equals('a'))
+                {
+                    Console.WriteLine("Your string contains 'a' ");
+                }
+                else if (input[i].Equals('A'))
+                {
+                    Console.WriteLine("Your string starts with 'A' ");
+                }
+                else
+                {
+                    Console.WriteLine("Your can't be considered");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
